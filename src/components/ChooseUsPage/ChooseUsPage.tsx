0010eb61d2028afe1647a40fae1e0a8bffbbd5dc
iphren <contact@iphren.com>
2024-10-30 14:50:42 +0000

    Add choose us page

--- src/components/ChooseUsPage/ChooseUsPage.tsx
+++ src/components/ChooseUsPage/ChooseUsPage.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Container, Typography } from "@mui/material";
+import { useTranslation } from 'react-i18next';
 
 const ChooseUsPage = () => {
-    return <div>
-        <h1>Choose Us Page</h1>
-    </div>;
+    const { t } = useTranslation();
+
+    return <Container sx={{
+        my: 5,
+        px: 5
+    }}>
+        <Typography variant="h4" sx={{ mb: 5 }} gutterBottom>
+            {t('uk_online_courses.content.choose_us.title')}
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
+            {t('uk_online_courses.content.choose_us.content.faculty')}
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
+            {t('uk_online_courses.content.choose_us.content.faculty_desc')}
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
+            {t('uk_online_courses.content.choose_us.content.students')}
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
+            {t('uk_online_courses.content.choose_us.content.students_desc')}
+        </Typography>
+    </Container>;
 }
 
 export default ChooseUsPage;
