3ef1b875e14176640e54866cf5ff829b8d809ac1
iphren <contact@iphren.com>
2024-10-30 22:05:42 +0000

    Finish footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { useTranslation } from 'react-i18next';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
+    const { t } = useTranslation();
+
     return <Container sx={{
         my: 5,
         px: 5,
         textAlign: "center"
     }}>
-        footer
+        <div className={styles.row}>
+            <div className={styles.wechat1}>
+                <Typography variant="button">
+                    {t('wechat_public')}
+                </Typography>
+                <div className={`${styles.qrcode} ${styles.image}`}></div>
+            </div>
+            <div className={styles.wechat2}>
+                <Typography variant="button">
+                    {t('wechat_helper')}
+                </Typography>
+                <div className={`${styles.qrcode} ${styles.image}`}></div>
+            </div>
+            <div>
+                <div className={styles.contact}>
+                    <Typography variant="button">{t('email')}</Typography>
+                    <Typography variant="body1">info@inedu.online</Typography>
+                </div>
+                <div className={styles.contact}>
+                    <Typography variant="button">{t('phone')}</Typography>
+                    <Typography variant="body1">+44 (0) 776 3755 435</Typography>
+                </div>
+            </div>
+        </div>
+        <div className={styles.row}>
+            <div>
+                <div className={`${styles.englishUk} ${styles.image}`}></div>
+            </div>
+            <div>
+                <div className={`${styles.britishCouncil} ${styles.image}`}></div>
+            </div>
+        </div>
     </Container>;
 }
 
