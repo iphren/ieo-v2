874f6d573ea004958e3b0c1a7bb74436cdea7a5c
iphren <contact@iphren.com>
2024-10-30 14:51:27 +0000

    Rename test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import {MemoryRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
+test('renders home page button', () => {
   render(<MemoryRouter><App /></MemoryRouter>);
   const linkElement = screen.getByText(/home_page/i);
   expect(linkElement).toBeInTheDocument();
