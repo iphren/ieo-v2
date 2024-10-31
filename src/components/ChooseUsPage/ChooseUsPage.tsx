99327167eec3869c70383775aa80a0c8ae94e561
iphren <contact@iphren.com>
2024-10-31 00:00:49 +0000

    Add tutors to tutors page

--- src/components/ChooseUsPage/ChooseUsPage.tsx
+++ src/components/ChooseUsPage/ChooseUsPage.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import { Container, Typography } from "@mui/material";
 import { useTranslation } from 'react-i18next';
+import Title from "../../Title";
 
 const ChooseUsPage = () => {
     const { t } = useTranslation();
 
-    return <Container fixed={true} sx={{
-        my: 5,
-        px: 5
-    }}>
-        <Typography variant="h4" sx={{ mb: 5 }} gutterBottom>
-            {t('uk_online_courses.content.choose_us.title')}
-        </Typography>
-        <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
-            {t('uk_online_courses.content.choose_us.content.faculty')}
-        </Typography>
-        <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
-            {t('uk_online_courses.content.choose_us.content.faculty_desc')}
-        </Typography>
-        <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
-            {t('uk_online_courses.content.choose_us.content.students')}
-        </Typography>
-        <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
-            {t('uk_online_courses.content.choose_us.content.students_desc')}
-        </Typography>
-    </Container>;
+    return <>
+        <Title title={t('uk_online_courses.content.choose_us.title')} />
+        <Container fixed={true} sx={{
+            my: 5,
+            px: 5
+        }}>
+            <Typography variant="h4" sx={{ mb: 5 }} gutterBottom>
+                {t('uk_online_courses.content.choose_us.title')}
+            </Typography>
+            <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
+                {t('uk_online_courses.content.choose_us.content.faculty')}
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
+                {t('uk_online_courses.content.choose_us.content.faculty_desc')}
+            </Typography>
+            <Typography variant="h5" sx={{ mb: 4 }} gutterBottom>
+                {t('uk_online_courses.content.choose_us.content.students')}
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 4 }} gutterBottom>
+                {t('uk_online_courses.content.choose_us.content.students_desc')}
+            </Typography>
+        </Container>
+    </>;
 }
 
 export default ChooseUsPage;
--- src/components/TutorsPage/TutorsPage.tsx
+++ src/components/TutorsPage/TutorsPage.tsx
@@ -1,18 +1,84 @@
 import React from 'react';
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Card, CardMedia, CardContent } from "@mui/material";
+import Grid from '@mui/material/Grid2';
 import { useTranslation } from 'react-i18next';
+import Title from "../../Title";
 
 const TutorsPage = () => {
     const { t } = useTranslation();
+    const translate = (tutorKey: string) => t(
+        'uk_online_courses.content.tutors.content.' + tutorKey,
+        { returnObjects: true }) as { name: string, intro: string, subjects: string };
+    const tutors = [
+        {
+            translation: translate('wu'),
+            image: '/img/people/ieo-wu.jpg'
+        },
+        {
+            translation: translate('jason'),
+            image: '/img/people/ieo-jason.jpg'
+        },
+        {
+            translation: translate('huang'),
+            image: '/img/people/ieo-huang.jpg'
+        },
+        {
+            translation: translate('phoebe'),
+            image: '/img/people/ieo-phoebe.jpg'
+        },
+        {
+            translation: translate('selena'),
+            image: '/img/people/ieo-selena.jpg'
+        },
+        {
+            translation: translate('maddy'),
+            image: '/img/people/ieo-maddy.jpg'
+        },
+        {
+            translation: translate('christine'),
+            image: '/img/people/ieo-christine.png'
+        }];
+    const tutorCardSize = {
+        xs: 12,
+        sm: 6,
+        md: 4,
+        lg: 3,
+    };
 
-    return <Container fixed={true} sx={{
-        my: 5,
-        px: 5
-    }}>
-        <Typography variant="h4" sx={{ mb: 5 }} gutterBottom>
-            {t('uk_online_courses.content.tutors.title')}
-        </Typography>
-    </Container>;
+    return <>
+        <Title title={t('uk_online_courses.content.tutors.title')} />
+        <Container fixed={true} sx={{
+            my: 5,
+            px: 5
+        }}>
+            <Typography variant="h4" sx={{ mb: 5 }} gutterBottom>
+                {t('uk_online_courses.content.tutors.title')}
+            </Typography>
+            <Grid container spacing={2} justifyContent="center">
+                {tutors.map((tutor) => (
+                    <Grid size={tutorCardSize} key={tutor.image}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image={tutor.image}
+                                title={tutor.translation.name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    {tutor.translation.name}
+                                </Typography>
+                                <Typography variant="body1" sx={{ mb: 1 }}>
+                                    {tutor.translation.intro}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {tutor.translation.subjects}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>))}
+            </Grid>
+        </Container>
+    </>;
 }
 
 export default TutorsPage;
