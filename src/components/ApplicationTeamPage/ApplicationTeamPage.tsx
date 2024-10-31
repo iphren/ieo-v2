dc9c94a2e69beea7a2f57a29bb991f32d16702a5
iphren <contact@iphren.com>
2024-10-31 11:32:05 +0000

    Remove unused dependencies

--- src/components/ApplicationTeamPage/ApplicationTeamPage.tsx
+++ src/components/ApplicationTeamPage/ApplicationTeamPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Typography, Card, CardMedia, CardContent } from "@mui/material";
-import Grid from '@mui/material/Grid2';
 import { useTranslation } from 'react-i18next';
 import Title from "../../Title";
 
