be206e59fcc3931ede2101f88ab6941435f14746
iphren <contact@iphren.com>
2024-10-31 16:24:40 +0000

    Add resources types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,23 @@ export interface Rankings {
         "zh-cn": string;
     }[];
 }
+
+export interface Translation {
+    "zh-CN": string;
+    "en-GB": string;
+}
+
+export interface Feed {
+    title: Translation;
+    description: Translation;
+    site: Translation;
+    author: Translation;
+    img: string;
+    date: string;
+    url: string;
+}
+
+export interface AdmissionLetter {
+    img: string;
+    university: Translation;
+}
