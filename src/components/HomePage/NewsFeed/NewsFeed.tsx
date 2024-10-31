b83c69a0cd27d46ab0ca2714b6348cb8bcae4496
iphren <contact@iphren.com>
2024-10-31 17:13:43 +0000

    Import JSON files instead of fetching

--- src/components/HomePage/NewsFeed/NewsFeed.tsx
+++ src/components/HomePage/NewsFeed/NewsFeed.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Container, Typography } from "@mui/material";
 import { useTranslation } from 'react-i18next';
+import { Feed } from "../../../types";
+import news from "../../../data/news.json";
 
 const NewsFeed = () => {
     const { t } = useTranslation();
+    const newsFeed: Feed[] = news;
+    console.log(newsFeed);
 
     return <Container fixed={true} sx={{
         my: 5,
