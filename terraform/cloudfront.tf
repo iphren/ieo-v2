cfcf6e57f75c4f0e00e4b7b59b01639d5e7a9dc2
iphren <contact@iphren.com>
2024-11-01 19:02:00 +0000

    Add terraform configs for CloudFront

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,61 @@
+locals {
+  s3_origin_id   = "${var.app_bucket}-origin"
+  s3_domain_name = "${var.app_bucket}.s3-website-${var.aws_region}.amazonaws.com"
+}
+
+resource "aws_cloudfront_distribution" "app_cdn" {
+  enabled = true
+
+  origin {
+    domain_name              = local.s3_domain_name
+    origin_id                = local.s3_origin_id
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    min_ttl                = "0"
+    default_ttl            = "300"
+    max_ttl                = "1200"
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = "200"
+    response_page_path    = "/404.html"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Project = var.project
+    AppId = var.app_id
+  }
+}
