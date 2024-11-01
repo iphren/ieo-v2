c7909f147b42545a5c41a4395cb57dd1eba063ae
iphren <contact@iphren.com>
2024-11-01 19:43:39 +0000

    Add content type to static files

--- terraform/dist.tf
+++ terraform/dist.tf
@@ -2,10 +2,17 @@ locals {
   dist_filepath = "../dist"
 }
 
+module "template_files" {
+  source = "hashicorp/dir/template"
+  base_dir = "${path.module}/${local.dist_filepath}"
+}
+
 resource "aws_s3_object" "dist" {
-  for_each = fileset(local.dist_filepath, "**")
+  for_each = module.template_files.files
   bucket = aws_s3_bucket.app_bucket.id
   key    = each.key
-  source = "${local.dist_filepath}/${each.value}"
-  etag   = filemd5("${local.dist_filepath}/${each.value}")
+  content_type = each.value.content_type
+  source  = each.value.source_path
+  content = each.value.content
+  etag = each.value.digests.md5
 }
