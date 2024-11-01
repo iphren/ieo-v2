9512c7afa99a5d503d5b3483510f7737ad5b8883
iphren <contact@iphren.com>
2024-11-01 19:04:42 +0000

    Update S3 access policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -25,3 +25,26 @@ resource "aws_s3_bucket_ownership_controls" "app_bucket" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+
+resource "aws_s3_bucket_policy" "app_bucket" {
+  bucket = aws_s3_bucket.app_bucket.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.app_bucket.arn}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : aws_cloudfront_distribution.app_cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
