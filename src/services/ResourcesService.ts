37b203416671f192058c2a631ef41edd6cd4aff2
iphren <contact@iphren.com>
2024-11-03 00:46:05 +0000

    Replace hardcoded API url

--- src/services/ResourcesService.ts
+++ src/services/ResourcesService.ts
@@ -1,15 +1,17 @@
 import axios, { AxiosResponse } from 'axios';
 import { Message, Rankings } from "../types";
 
+const baseUrl = import.meta.env.VITE_IEO_API_URL || "/api";
+
 export const getRankings = async () => {
     const response = await axios.get<Rankings[]>(
-        'https://d1yfas0kwd4c6z.cloudfront.net/ieo/rankings');
+        `${baseUrl}/rankings`);
     return response.data;
 }
 
 export const sendMessage = async (data: Message) => {
     await axios.post<null, AxiosResponse<null>, Message>(
-        'https://d1yfas0kwd4c6z.cloudfront.net/ieo/messages',
+        `${baseUrl}/messages`,
         data
     );
 }
