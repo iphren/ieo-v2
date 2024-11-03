d54034f7afd5984b136854980b8b8db24cee3328
iphren <contact@iphren.com>
2024-11-03 03:01:51 +0000

    Move terraform backend configs to env vars

--- terraform/backends.tf
+++ terraform/backends.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "configs.inedu.online"
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    profile        = "ieo"
-    encrypt        = true
+    encrypt = true
   }
 }
